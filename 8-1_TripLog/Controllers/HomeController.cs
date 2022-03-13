203c52cd6aff4ca71c5c6b6c1f85023fb75b0571
Gotterbote <got1ssei@msn.com>
2022-03-13 18:34:26 -0500

    Add project files.

---
+++ 8-1_TripLog/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using _8_1_TripLog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace _8_1_TripLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TripLogContext context { get; set; }
+        public HomeController(TripLogContext ctx) => context = ctx;
+
+        public ViewResult Index()
+        {
+            var trips = context.Trips.OrderBy(t => t.StartDate).ToList();
+            return View(trips);
+        }
+    }
+}
---
+++ 8-1_TripLog/Migrations/20220313221618_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _8_1_TripLog.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    TripID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Destination = table.Column<string>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Accommodation = table.Column<string>(nullable: true),
+                    AccommodationPhone = table.Column<string>(nullable: true),
+                    AccommodationEmail = table.Column<string>(nullable: true),
+                    ThingToDo1 = table.Column<string>(nullable: true),
+                    ThingToDo2 = table.Column<string>(nullable: true),
+                    ThingToDo3 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.TripID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trips");
+        }
+    }
+}
